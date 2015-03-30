614e9f364e6898d13bf65615670572e17d0fb9c5
Rick van Ewijk <rickvanewijk@live.nl>
2015-03-30 11:04:48 +0200

    [Added some changes files]

---
+++ build/Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		compass: {                  // Task
+			dist: {                   // Target
+				options: {              // Target options
+					sassDir: 'scss',
+					cssDir: '../css',
+					environment: 'production'
+				}
+			},
+			dev: {                    // Another target
+				options: {
+					sassDir: 'scss',
+					cssDir: '../css'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['scss/**/*.scss'],
+				tasks: ['compass'],
+				options: {
+					spawn: false
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['compass', 'watch']);
+}
+
